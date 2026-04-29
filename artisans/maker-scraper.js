88ba2e1529bdaf0cbf649112bddca6bb0e6213d2
Anh Thang Bui <buianhthang89@gmail.com>
2026-04-29 19:34:56 +0700

    fix: sync images for maker scraper

--- artisans/maker-scraper.js
+++ artisans/maker-scraper.js
@@ -3,7 +3,12 @@ require('dotenv').config()
 const fs = require('fs')
 const files = fs.readdirSync('./artisans/makers')
 const Promise = require('bluebird')
-const { updateMakerDatabase, setDryRun } = require('./utils/database')
+const {
+    makeImageId,
+    updateMakerDatabase,
+    setDryRun,
+} = require('./utils/database')
+const { uploadImage, getListImages } = require('../utils/image')
 
 /**
  * List of makers that are known to have incomplete data
@@ -18,17 +23,71 @@ if (isDryRun) {
     setDryRun(true)
 }
 
+let existedImages = []
+
+const DELIVERY_BASE_URL = `https://imagedelivery.net/${process.env.CF_IMAGES_ACCOUNT_HASH}`
+
+const normalizeColorwayImage = (colorway) => {
+    if (!colorway.img || colorway.img.includes(DELIVERY_BASE_URL)) {
+        return colorway
+    }
+
+    return {
+        ...colorway,
+        remote_img: colorway.img,
+        img: `${DELIVERY_BASE_URL}/${makeImageId(colorway)}/public`,
+    }
+}
+
+const normalizeTables = (tables) =>
+    tables.map((table) => {
+        const colorways = table.colorways.map(normalizeColorwayImage)
+
+        return {
+            ...table,
+            img: colorways[0]?.img || table.img || null,
+            colorways,
+        }
+    })
+
+const syncImages = async (colorways) => {
+    const images = colorways
+        .filter((colorway) => colorway.remote_img)
+        .map((colorway) => [makeImageId(colorway), colorway.remote_img])
+        .filter(([filename]) => !existedImages.includes(filename))
+
+    if (!images.length) {
+        return
+    }
+
+    console.log('syncing images', images.length)
+
+    await Promise.map(images, async ([filename, url]) => {
+        await uploadImage(filename, url)
+        existedImages.push(filename)
+    }, { concurrency: 5 })
+}
+
 function scan(filename) {
     console.log(`🚀 Running specific maker scraper: ${filename}...`)
 
     const { scraper } = require(`./makers/${filename}`)
     const maker_id = filename.replace(/\.js$/, '')
 
-    return scraper().then((tables) =>
-        updateMakerDatabase(tables, {
-            preserve_missing: PARTIAL_MAKERS.has(maker_id),
+    return scraper()
+        .then(normalizeTables)
+        .then((tables) =>
+            updateMakerDatabase(tables, {
+                preserve_missing: PARTIAL_MAKERS.has(maker_id),
+            })
+        )
+        .then(async ({ colorways }) => {
+            await syncImages(colorways)
         })
-    )
 }
 
-Promise.map(files, scan, { concurrency: 1 })
+getListImages().then(async (images) => {
+    existedImages = images
+
+    await Promise.map(files, scan, { concurrency: 1 })
+})
